e4bbdbd149206b617ded9473541aea2903994fee
taehoryu <30508125+taehoryu@users.noreply.github.com>
2025-07-22 08:17:32 -0600

    Update test.py

--- test.py
+++ test.py
@@ -4,6 +4,10 @@ from streamlit_gsheets import GSheetsConnection
 # Create a connection object.
 conn = st.connection("gsheets", type=GSheetsConnection)
 
+student = st.text_input("Enter your name (or ID):")
+step = st.number_input("Step number", min_value=0, step=1)
+step_value = st.radio("Choose step", options=[1, -1], horizontal=True)
+
 df = conn.read()
 
 # Print results.
